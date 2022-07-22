6f9bc8ca67a883b48c66cca4de2c5c03e91ecd55
ardalan-di <ardalandelfardi@gmail.com>
2022-07-22 18:30:52 +0430

    small project start

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,11 +1,5 @@
-import Todo from "./components/Todo";
-
 function App() {
   return (<div>
-      <h1>My Todos</h1>
-      <Todo text = "learn React" />
-      <Todo text = "Master React" />
-      <Todo text = "Explore the full React course" />
 
     </div>
   );
