455816758f3f724d4986a1ce89ce5a2b1dcaabc8
ardalan-di <ardalandelfardi@gmail.com>
2022-07-30 18:24:39 +0430

    Update Favorites.js

--- react-course/src/pages/Favorites.js
+++ react-course/src/pages/Favorites.js
@@ -1,7 +1,25 @@
+import {useState} from 'react';
+
+let number = 0;
+
 function FavoritesPage()
 {
+    
+    const[current,setcurrent] = useState(number);
+    
+    function NumbHandler()
+    {
+        // number +=1;
+        console.log(number);
+        setcurrent(number+=1);
+    }
+
     return(
-        <div>Favorites Page</div>
+        <div>
+            
+            <h1>{current}</h1>
+            <button onClick={NumbHandler}>Click Me!</button>
+        </div>
     );
 }
 
